4533b97ea790f824f178af301b0b24cf681d3742
osama sabry <osamasabry@msn.com>
2026-09-05 19:38:06 +0300

    feat(network): switch default API base URL to live staging https://staging.modeefe.com/api

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -3,20 +3,21 @@ enum Environment { dev, staging, prod }
 class AppConfig {
   AppConfig._();
 
-  static Environment currentEnvironment = Environment.dev;
+  static Environment currentEnvironment = Environment.staging;
 
   static String get baseUrl {
     switch (currentEnvironment) {
       case Environment.dev:
         return 'http://127.0.0.1:8000/api';
       case Environment.staging:
-        return 'https://staging.modeefe.sa/api';
+        return 'https://staging.modeefe.com/api';
       case Environment.prod:
-        return 'https://api.modeefe.sa/api';
+        return 'https://staging.modeefe.com/api';
     }
   }
 
-  static const Duration connectTimeout = Duration(seconds: 3);
-  static const Duration receiveTimeout = Duration(seconds: 3);
+  static const Duration connectTimeout = Duration(seconds: 10);
+  static const Duration receiveTimeout = Duration(seconds: 10);
 }
+
 
